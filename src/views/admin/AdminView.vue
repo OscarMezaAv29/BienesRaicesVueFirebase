c32dfa1fd720488c6ccb3a43847a04127ce15dec
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-09-15 11:25:35 -0600

    first commit

---
+++ src/views/admin/AdminView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
+
+  <v-btn
+    color="blue"
+    varian="flat"
+    :to="{name: 'nueva-propiedad'}"
+  >
+    Nueva Propiedad
+  </v-btn>
+</template>
