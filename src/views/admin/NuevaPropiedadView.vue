482c221552525ce86376eef0e2adbe8d76148662
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-09-15 12:27:46 -0600

    Subir y mostrar imagenes

--- src/views/admin/NuevaPropiedadView.vue
+++ src/views/admin/NuevaPropiedadView.vue
@@ -4,8 +4,11 @@ import { collection, addDoc } from "firebase/firestore";
 import { useFirestore } from "vuefire";
 import { useRouter } from "vue-router";
 import { validationSchema, imageSchema } from "@/validation/propiedadSchema";
+import useImage from "@/composables/useImages";
 
 const items = [1, 2, 3, 4, 5];
+
+const { url, uploadImage, image } = useImage();
 
 const router = useRouter();
 const db = useFirestore();
@@ -33,6 +36,7 @@ const submit = handleSubmit(async (values) => {
 
   const docRef = await addDoc(collection(db, "propiedades"), {
     ...propiedad,
+    imagen: url.value
   });
   if (docRef.id) {
     router.push({ name: "admin-propiedades" });
@@ -50,31 +54,74 @@ const submit = handleSubmit(async (values) => {
     </v-card-subtitle>
 
     <v-form class="mt-10">
-      <v-text-field class="mb-5" label="Título Propiedad" v-model="titulo.value.value"
-        :error-messages="titulo.errorMessage.value" />
+      <v-text-field
+        class="mb-5"
+        label="Título Propiedad"
+        v-model="titulo.value.value"
+        :error-messages="titulo.errorMessage.value"
+      />
 
-      <v-file-input accept="image/jpeg" label="Fotografía" prepend-icon="mdi-camera" class="mb-5"
-        v-model="imagen.value.value" :error-messages="imagen.errorMessage.value" />
+      <v-file-input
+        accept="image/jpeg"
+        label="Fotografía"
+        prepend-icon="mdi-camera"
+        class="mb-5"
+        v-model="imagen.value.value"
+        :error-messages="imagen.errorMessage.value"
+        @change="uploadImage"
+      />
 
-      <v-text-field class="mb-5" label="Precio" v-model="precio.value.value"
-        :error-messages="precio.errorMessage.value" />
+      <div v-if="image" class="my-5">
+        <p class="font-weight-bold">Imagen Propiedad:</p>
+        <img class="w-50" :src="image" />
+      </div>
+
+      <v-text-field
+        class="mb-5"
+        label="Precio"
+        v-model="precio.value.value"
+        :error-messages="precio.errorMessage.value"
+      />
       <v-row>
         <v-col cols="12" md="4">
-          <v-select label="Habitaciones" class="mb-5" :items="items" v-model="habitaciones.value.value"
-            :error-messages="habitaciones.errorMessage.value" />
+          <v-select
+            label="Habitaciones"
+            class="mb-5"
+            :items="items"
+            v-model="habitaciones.value.value"
+            :error-messages="habitaciones.errorMessage.value"
+          />
         </v-col>
         <v-col cols="12" md="4">
-          <v-select label="WC" class="mb-5" :items="items" v-model="wc.value.value"
-            :error-messages="wc.errorMessage.value" />
+          <v-select
+            label="WC"
+            class="mb-5"
+            :items="items"
+            v-model="wc.value.value"
+            :error-messages="wc.errorMessage.value"
+          />
         </v-col>
         <v-col cols="12" md="4">
-          <v-select label="Lugares Estacionamiento" class="mb-5" :items="items" v-model="estacionamiento.value.value"
-            :error-messages="estacionamiento.errorMessage.value" />
+          <v-select
+            label="Lugares Estacionamiento"
+            class="mb-5"
+            :items="items"
+            v-model="estacionamiento.value.value"
+            :error-messages="estacionamiento.errorMessage.value"
+          />
         </v-col>
       </v-row>
-      <v-textarea class="mb-5" label="Descripción" v-model="descripcion.value.value"
-        :error-messages="descripcion.errorMessage.value"></v-textarea>
-      <v-checkbox label="Alberca" v-model="alberca.value.value" :error-messages="alberca.errorMessage.value" />
+      <v-textarea
+        class="mb-5"
+        label="Descripción"
+        v-model="descripcion.value.value"
+        :error-messages="descripcion.errorMessage.value"
+      ></v-textarea>
+      <v-checkbox
+        label="Alberca"
+        v-model="alberca.value.value"
+        :error-messages="alberca.errorMessage.value"
+      />
 
       <v-btn color="pink-accent-3" block @click="submit">
         Agregar Propiedad
